8bec746ea1a8dde175de8f2a7346b53419c7812e
Abhay Mathur <abhaymathur629@gmail.com>
2026-05-23 18:35:53 +0530

    Update urls.py

--- awning/urls.py
+++ awning/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from website.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
 ]
