23eefcd38edeea6aca30e1fb6030070a0aa90e79
hanji-abhay <abhaymathur629@gmail.com>
2026-05-24 08:44:23 +0530

    SEO: Add robots.txt route mapping

--- awning/urls.py
+++ awning/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 from website.sitemaps import StaticViewSitemap
 
 sitemaps = {
@@ -26,5 +27,6 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
